55b66c0c0fdabe98958386d947b1b069e01274e7
kktofu720 <rlaalswn0209@naver.com>
2023-12-12 18:10:48 +0900

    1212 민주 간트차트 html

--- laravel_together/resources/views/ganttchart.blade.php
+++ laravel_together/resources/views/ganttchart.blade.php
@@ -6,30 +6,38 @@
 @section('main')
 	<div class="gantt-content-wrap">
 		<div class="gantt-btn-wrap">
-			<input type="search" name="" id="" value="업무명, 업무번호 검색">
+			<input class="gantt-search" type="search" name="" id="" value="업무명, 업무번호 검색">
 			<ul class="gantt-btn-drop">
-				<li>상태</li>
-				<li>우선순위</li>
-				<li>담당자</li>
-				<li>시작일</li>
-				<li>마감일</li>
-				<button class="gantt-add-btn">업무추가</button>
+				<li class="gantt-li">상태</li>
+				<li class="gantt-li">우선순위</li>
+				<li class="gantt-li">담당자</li>
+				<li class="gantt-li">시작일</li>
+				<li class="gantt-li">마감일</li>
 			</ul>
+			<button class="gantt-add-btn">업무추가</button>
 		</div>
-		<div class="gantt-task-wrap">
-			<div class="gantt-task-header">
-				<div>업무명</div>
-				<div>담당자</div>
-				<div>상태</div>
-				<div>시작일</div>
-				<div>마감일</div>
+		<section class="gantt-task-wrap">
+			<div>
+				<div class="gantt-task-header">
+					<div>업무명</div>
+					<div>담당자</div>
+					<div>상태</div>
+					<div>시작일</div>
+					<div>마감일</div>
+				</div>
+				<div>
+
+				</div>
 			</div>
+			<div>
+				<div class="gantt-chart-wrap">
+
+				</div>
+			</div>
+			
 			<div>
 
 			</div>
-		</div>
-		<div class="gantt-chart-wrap">
-
-		</div>
+		</section>	
 	</div>
 @endsection
