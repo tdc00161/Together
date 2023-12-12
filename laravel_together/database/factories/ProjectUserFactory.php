e5588ca656d311d17d1a9d5e9bdeb7d0cadffaff
tdc00161 <tdc00161@naver.com>
2023-12-12 11:06:05 +0900

    231212 model factory update

--- laravel_together/database/factories/ProjectUserFactory.php
+++ laravel_together/database/factories/ProjectUserFactory.php
@@ -18,9 +18,9 @@ class ProjectUserFactory extends Factory
     {
         $date = $this->faker->dateTimeBetween('-1 years');
         return [
-            'project_id'=>$this->faker->randomNumber(4) // 프로젝트 pk
-            'authority_id'=>$this->faker->numberBetween(1,2) // 권한 pk
-            'member_id'$this->faker->randomNumber(2) // 참여자 pk
+            'project_id'=>$this->faker->randomNumber(4), // 프로젝트 pk
+            'authority_id'=>$this->faker->numberBetween(1,2), // 권한 pk
+            'member_id'=>$this->faker->randomNumber(2), // 참여자 pk
             'created_at'=> $date, // 작성일자
             'update_at'=> $date, // 수정일자
         ];
