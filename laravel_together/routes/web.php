0061788dd5b7a0991264926b6e2601e13b75a851
jueunyang08 <sa04129@naver.com>
2023-12-12 10:02:23 +0900

    1212 공통 레이아웃 및 각 블레이드 생성 / 라우터 정의

--- laravel_together/routes/web.php
+++ laravel_together/routes/web.php
@@ -13,6 +13,37 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/layout', function () {
     return view('layout.layout');
 });
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+Route::get('/ganttchart', function () {
+    return view('ganttchart');
+});
+Route::get('/project-create', function () {
+    return view('/project-create');
+});
+Route::get('/project-individual', function () {
+    return view('/project-individual');
+});
+Route::get('/project-team', function () {
+    return view('/project-team');
+});
+
+
+// 모달
+Route::get('/detail', function () {
+    return view('modal/detail');
+});
+Route::get('/insert', function () {
+    return view('modal/insert');
+});
+Route::get('/messenger', function () {
+    return view('modal/messenger');
+});
+
+
+
+
