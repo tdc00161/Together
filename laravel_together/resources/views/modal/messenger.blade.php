92f26b2496039c615b2b4676b93174a669177a67
jueunyang08 <sa04129@naver.com>
2023-12-15 15:37:35 +0900

    1215 messenger modal 중간 저장

--- laravel_together/resources/views/modal/messenger.blade.php
+++ laravel_together/resources/views/modal/messenger.blade.php
@@ -2,9 +2,9 @@
     <link rel="stylesheet" href="/css/messenger.css">
 </head>
 <body>
-    <div id="m-myModal" class="modal">
-    <div class="modal-content">
-        <span class="modal-close" onclick="closeModal()">&times;</span>
+    <div id="m-myModal" class="m-modal">
+    <div class="m-modal-content">
+        <span class="m-modal-close" onclick="mcloseModal()">&times;</span>
        <!-- 탭 목록 -->
 <ul class="tabs">
     <li class="tab tab-active" onclick="openTab('tab1')"><img class="tab-icon" src="/img/icon-Subtract.png" alt=""></li>
@@ -20,20 +20,59 @@
 </div>
 
 <div id="tab2" class="tab-content">
-    <h3>탭 2 내용</h3>
-    <p>이곳에 탭 2의 내용이 들어갑니다.</p>
-</div>
+    
 
-<div id="tab3" class="tab-content">
-    <h3>탭 3 내용</h3>
-    <p>이곳에 탭 3의 내용이 들어갑니다.</p>
-</div>
+    {{-- 친구 검색 bar --}}
+    <div class="friend-searchbar-div">
+    <div class="friend-searchbar">
+        <input type="search" placeholder="친구 검색"> 
+    <button class="search-icon"><img src="/img/icon-search.png" alt=""></button>
+    </div>
+    {{-- 친구 추가 버튼 --}}
+    <button class="add-button" type="button" onclick="openModal()">+</button>
+    
+  </div>
 
-<div id="tab4" class="tab-content">
-    <h3>탭 4 내용</h3>
-    <p>이곳에 탭 4의 내용이 들어갑니다.</p>
+    
+    {{-- side title : 친구요청 --}}
+    <div class="m-side-title-div"><p class="m-side-title">친구 요청</p><div class="notice-count">2</div></div>
+    
+    <div class="messenger-user-div m-received-bg">
+        <div class="user-profile"><img src="/img/profile-img.png" alt=""></div>
+        <p class="user-text">누구셈</p>
+        <p class="user-text">gffds@naver.com</p>
+        <button class="accept-btn">수락</button>
+        <button class="refuse-btn">거절</button>
+    </div>
+
+    <div class="messenger-user-div m-received-bg">
+        <div class="user-profile"><img src="/img/profile-img.png" alt=""></div>
+        <p class="user-text">이이잉</p>
+        <p class="user-text">gffds@naver.com</p>
+        <button class="accept-btn">수락</button>
+        <button class="refuse-btn">거절</button>
+    </div>
+
+    <div class="messenger-user-div m-request-bg">
+        <div class="user-profile"><img src="/img/profile-img.png" alt=""></div>
+        <p class="user-text">이이잉</p>
+        <p class="user-text">fddefds@naver.com</p>
+        <button class="request-cancle-btn">요청취소</button>
+    </div>
+
+    {{-- side title : 현재활동중 --}}
+    <div class="m-side-title-div"><p class="m-side-title">현재활동중</p></div>
 </div>
     </div>
 </div>
+
+{{-- 친구추가 모달 --}}
+<div id="friend-Modal">
+    <div class="friend-Modal-content">
+        <button onclick="fcloseModal()">X</button>
+        <div><input type="text" placeholder="email로 검색"></div>
+    </div>
+</div>
+
 <script src="/js/messenger.js"></script>
 </body>
