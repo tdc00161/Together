00f61114948e6868bcfb5bb65c3ab7d47abe4ade
Dokal22 <stly3466@gmail.com>
2023-12-15 10:00:08 +0900

    메신저 기본 액티브 css 설정

--- laravel_together/resources/views/modal/messenger.blade.php
+++ laravel_together/resources/views/modal/messenger.blade.php
@@ -7,7 +7,7 @@
         <span class="modal-close" onclick="closeModal()">&times;</span>
        <!-- 탭 목록 -->
 <ul class="tabs">
-    <li id="defaulttab" class="tab" onclick="openTab('tab1')"><img class="tab-icon" src="/img/icon-Subtract.png" alt=""></li>
+    <li class="tab tab-active" onclick="openTab('tab1')"><img class="tab-icon" src="/img/icon-Subtract.png" alt=""></li>
     <li class="tab" onclick="openTab('tab2')"><img class="tab-icon" src="/img/icon-group.png" alt=""></li>
     {{-- <li class="tab" onclick="openTab('tab3')">탭 3</li>
     <li class="tab" onclick="openTab('tab4')">탭 4</li> --}}
