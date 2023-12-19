83269d05a6b5ade4d8fb664dbcddaa89a7cb220b
tdc00161 <tdc00161@naver.com>
2023-12-19 19:29:07 +0900

    231219 controller 작업

--- laravel_together/app/Http/Controllers/ProjectController.php
+++ laravel_together/app/Http/Controllers/ProjectController.php
@@ -21,46 +21,35 @@ class ProjectController extends Controller
         return view('project_create');
     }
 
-    public function mainstore(Request $request) {
+    public function maincreate(Request $request) {
+
+        $data= $request
+                ->only('user_pk','color_code_pk','project_title', 'project_content', 'flg', 'start_date', 'end_date');
+        
+        // user_id (session 에 저장된 값 호출)
         $user_id = Session::get('user')->only('id');
         // $user_id = 171;
         // dd($user_id);
+        
+        $data['user_pk'] = $user_id['id'];
 
+        // color_code 랜덤 호출
         $color_code = DB::table('basedata')
                         ->join('projects','color_code_pk','=','data_content_code')
                         // ->select('data_content_name')
-                        ->select('data_title_code')
+                        ->select('data_content_code')
                         ->where("data_title_code","=","3")
-                        ->orderByRaw('RAND()')
-                        ->first();
-        // dd($color_code->data_content_name);
+                        ->get();
 
-        // $color_coded = $color_code->shuffle();
-        // $color_array = $color_coded->all();
-        // foreach ($color_coded as $items) {
-        //     $color_array[] = color_code[$items]; 
-        // }
 
-        // $color_coded = $items[0,4].(mt_rand(0,3));
-          
-        // $color_coded = array_rand($color_code,count($color_code));
+        foreach ($color_code as $items) {
+          $data['color_code_pk'] = $items->data_content_code; //
+        }
 
-        
-
-        // dd($color_code);
-        $data= $request
-                ->only('user_pk','color_code_pk','project_title', 'project_content', 'flg', 'start_date', 'end_date');
-        $data['user_pk'] = $user_id['id'];
-        // $data['user_pk'] = $user_id;
-        // $data['color_code_pk'] = $color_code->data_content_name;
-        $data['color_code_pk'] = $color_code->data_title_code;
         // dd($data);
 
-        // $start = $data['start_date'];
-        // $start = Carbon::create($data['start_date']);
-        // $end = Carbon::create($data['end_date']);
-        // $dday[] = $start->diffInDays($end);
 
+        // dday 호출
         foreach ($data as $items) {
           $start = Carbon::create($data['start_date']);
           $end = Carbon::create($data['end_date']);
@@ -71,32 +60,69 @@ class ProjectController extends Controller
 
 
 
+        // dd($project_title);
+        // $project_content = $request->project_content');
+        // $start_date = $request->input('start_date');
+        // $end_date = $request->input('end_date');
+
         $result = Project::create($data);
-        // dd($result);
-        // DB::table('project')->insert([
-        //     'user_id' => '',
-        //     'project_title' => '',
-        //     'project_content' => '',
-        //     'flg' => '',
-        //     'stat_date' =>'',
-        //     'end_date' =>'',
-        // ]);
 
         if ($result->flg == '0'){
-          return redirect()->route('individual.get',['id' => $user_id['id']]);
+          return redirect()->route('individual.get',['user_pk' => $result['user_pk']]);
         } elseif ($result->flg == '1'){
-          return redirect()->route('team.get',['id' => $user_id['id']]);
-        } else {
-          return '다시 확인해주세요';
+          return redirect()->route('team.get',['user_pk' => $result['user_pk']]);
         }
+
+        return '다시 확인해주세요';
 
     }
 
 
-    public function mainshow($id) {
-        
-        $result = project::find($id);
+    public function mainshow($user_pk) {
+        // dd($request);
+      // find -> pk 호출만 가능
+        $result = DB::table('projects')
+                    -> select('id'
+                      ,'user_pk'
+                      ,'color_code_pk'
+                      ,'project_title'
+                      ,'project_content'
+                      ,'flg'
+                      ,'start_date'
+                      ,'end_date')
+                    -> where('user_pk', $user_pk)
+                    -> get();
+        // dd($result);
 
+        $project_title = $result[$items->project_title];
+        dd($project_title);
+
+        // $tkdata = DB::table('projects')
+        // -> select('tasks.id',
+        //           'projects.user_pk',
+        //           'tasks.project_id',
+        //           'tasks.task_responsible_id',
+        //           'tasks.title',
+        //           'tasks.content',
+        //           'tasks.category_id',
+        //           'tasks.start_date',
+        //           'tasks.end_date',
+        //           'base1.data_content_name as status_name',
+        //           'base2.data_content_name as category_name',
+        //           )
+        // -> join('tasks', function($join) {
+        //   $join->on('tasks.project_id','=','projects.id');
+        // })
+        // -> join('basedata as base1', function($base1){
+        //   $base1->on('base1.data_content_code','=','tasks.task_status_id');
+        // })
+        // -> join('basedata as base2', function($base2){
+        //   $base2->on('base2.data_content_code','=','tasks.category_id');
+        // })
+        // -> where('projects.user_pk', '=', $user_pk)
+        // -> where('base1.data_title_code', '=', '0')
+        // -> where('base2.data_title_code', '=', '2')
+        // -> get();
 
         $tkdata = DB::table('projects')
                       -> select('tasks.id',
@@ -112,53 +138,20 @@ class ProjectController extends Controller
                                 'base2.data_content_name as category_name',
                                 )
                       -> join('tasks', function($join) {
-                        $join->on('tasks.project_id','=','projects.id')->where('projects.user_pk', '=', 7);
+                        $join->on('tasks.project_id','=','projects.user_pk');
                       })
                       -> join('basedata as base1', function($base1){
-                        $base1->on('base1.data_content_code','=','tasks.task_status_id')->where('base1.data_title_code', '=', '0');
+                        $base1->on('base1.data_content_code','=','tasks.task_status_id');
                       })
                       -> join('basedata as base2', function($base2){
-                        $base2->on('base2.data_content_code','=','tasks.category_id')->where('base2.data_title_code', '=', '2');
+                        $base2->on('base2.data_content_code','=','tasks.category_id');
                       })
+                      -> where('projects.user_pk', '=', $user_pk)
+                      -> where('base1.data_title_code', '=', '0')
+                      -> where('base2.data_title_code', '=', '2')
                       -> get();
         // dd($tkdata);
-        // $projectdata = DB::table('projects')
-        //               -> select('projects.user_pk',
-        //                         'projects.start_date',
-        //                         'projects.end_date',
-        //                         'tasks.project_id',
-        //                         'tasks.task_responsible_id',
-        //                         'tasks.title',
-        //                         'tasks.content',
-        //                         'tasks.category_id'
-        //               )
-        //               -> join('tasks', function($join) {
-        //                 $join->on('tasks.project_id','=','projects.id')->where('projects.user_pk', '=', 7);
-        //               })
-        //               -> get();
-        // dd($projectdata);
 
-        // $statusdata = DB::table('tasks')
-        //                   -> select('data_content_name')
-        //                   -> join('basedata', function($base1){
-        //                     $base1->on('data_content_code','=','tasks.task_status_id')->where('data_title_code', '=', '0');
-        //                   })
-        //                   // -> join('basedata', function($base2){
-        //                   //   $base2->on('data_content_code','=','tasks.category_id')->where('data_title_code', '=', '2');
-        //                   // })
-        //                   -> get();
-        // // dd($statusdata);
-
-        // $categorydata = DB::table('tasks')
-        //                     -> select('data_content_name')
-        //                     // -> join('basedata', function($base1){
-        //                     //   $base1->on('data_content_code','=','tasks.task_status_id')->where('data_title_code', '=', '0');
-        //                     // })
-        //                     -> join('basedata', function($base2){
-        //                       $base2->on('data_content_code','=','tasks.category_id')->where('data_title_code', '=', '2');
-        //                     })
-        //                     -> get();
-          // dd($categorydata);
 
         // 업무 시작/마감일자 d-day 설정
         foreach ($tkdata as $items) {
@@ -167,17 +160,11 @@ class ProjectController extends Controller
           // $dday[] = $start->diffInDays($end);
           $items->dday = $start->diffInDays($end); // tkdata에 dday 추가
         }
-        // dd($tkdata);
 
-
-
-
-        // dd($result);
-
-        // $data2 = if ($tkdata->category_id = 0) {
-          
-        // }
+        dd($tkdata);
 
         return view('project_individual')->with('result',$result)->with('data',$tkdata);
     }
 }
+
+
