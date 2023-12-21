f64f139061e43a739f880c38a22dba3c3e9cff36
tdc00161 <tdc00161@naver.com>
2023-12-21 18:48:12 +0900

    231221 project individual 업데이트

--- laravel_together/app/Http/Controllers/ProjectController.php
+++ laravel_together/app/Http/Controllers/ProjectController.php
@@ -13,7 +13,6 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Session;
-use Illuminate\Support\Facades\Log;
 
 use Carbon\Carbon;
 
