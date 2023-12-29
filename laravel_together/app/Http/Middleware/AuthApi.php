f3e19f664300a0f11419a21a2bfe69db77714278
Dokal22 <stly3466@gmail.com>
2023-12-29 17:24:57 +0900

    api 미들웨어 fix

--- laravel_together/app/Http/Middleware/AuthApi.php
+++ laravel_together/app/Http/Middleware/AuthApi.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthApi
 {
@@ -16,10 +17,12 @@ class AuthApi
      */
     public function handle(Request $request, Closure $next)
     {
-        if ($request->header('Accept') == 'application/json' || $request->is('api/*')) { // 주소 시작이 /api/ 인지
-            return $next($request);
-        } else {
-            return abort(403, 'Unauthorized'); // HTTP 통신 중단 후 값 반환?
-        }
+        // if ($request->header('Accept') == 'application/json' || $request->is('api/*')) { // 주소 시작이 /api/ 인지
+            if (Auth::check()) {
+                return $next($request);
+            }
+            return abort(403, 'No login');
+        // }
+        // return abort(403, 'No json'); // HTTP 통신 중단 후 값 반환?        
     }
 }
