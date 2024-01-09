61752050b94af29e6601a1ac063d58a0bbf9c23c
Dokal22 <stly3466@gmail.com>
2024-01-09 18:10:35 +0900

    메신저 알림 중간저장

---
+++ laravel_together/app/Events/MessageCame.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\User;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
+class MessageCame
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $message, $senderName;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct($message)
+    {
+        $this->message = $message;
+        // $this->senderName = User::find($message->sender_id)->name;
+        $this->dontBroadcastToCurrentUser();
+        Log::debug('메세지온다');
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('chats');
+    }
+}
