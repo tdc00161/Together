d10af593de1d622750f7c1d048fbc6005cda1548
Dokal22 <stly3466@gmail.com>
2024-01-12 10:19:28 +0900

    온라인 중간저장

---
+++ laravel_together/app/Events/OnOffline.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
+class OnOffline
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $user;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct($user)
+    {
+        $this->user = $user;
+    }
+
+    // Who 온라인
+    public function whoOnline()
+    {
+        Log::debug('온라인');
+        Log::debug($this->user);
+    }
+
+    // Who 오프라인
+    public function whoOffline()
+    {
+        Log::debug('오프라인');
+        Log::debug($this->user);
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
