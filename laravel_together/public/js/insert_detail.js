17df80f7ec3b07683ebe914211966993a5c0d4bb
Dokal22 <stly3466@gmail.com>
2024-01-18 17:08:47 +0900

    대쉬보드->팀프로젝트진척->빈칸 오류 임시 수정

--- laravel_together/public/js/insert_detail.js
+++ laravel_together/public/js/insert_detail.js
@@ -921,6 +921,34 @@ function createTask() {
 		});
 }
 
+// 테스크 제목 찾는 함수
+function findTaskName(p) {		
+	function findModuleOne(Pone) {
+		let a = Array.from(Pone.children);
+		a.forEach((RTCone,RTCi)=>{
+			console.log('RTCone');
+			console.log(RTCone);
+			if(RTCone.classList.contains('taskName')){
+				return refreshTitle = RTCone;
+			} else {
+				if(RTCi === p.children.length - 1 && p.child ){
+					return findTaskName(RTCone);
+				}
+			}
+		})
+	}		
+	if(p.length !== 0){
+		let b = Array.from(p.children)
+		b.forEach((Pone,Pi)=>{
+			console.log('Pone');
+			console.log(Pone);
+			findModuleOne(Pone);
+		})
+	} else {
+		findModuleOne(p);
+	}
+}
+
 // 등록 버튼으로 작성/수정
 function updateTask() {
 
@@ -992,7 +1020,10 @@ function updateTask() {
 				let refreshResponsible = refreshTarget.firstElementChild.nextElementSibling.firstElementChild.firstElementChild
 				refreshResponsible.textContent = data.data.names.task_responsible_name
 				// 해당 간트 제목
-				let refreshTitle = refreshTarget.firstElementChild.firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling
+				let refreshTitle = '';
+				console.log(findTaskName(refreshTarget));
+				findTaskName(refreshTarget);
+
 				refreshTitle.textContent = data.data.task.title
 				// 해당 간트 시작일				
 				let refreshStart = refreshTarget.firstElementChild.nextElementSibling.firstElementChild.nextElementSibling.nextElementSibling.firstElementChild
